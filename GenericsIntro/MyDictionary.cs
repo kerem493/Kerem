abd5f135f80963dc2eeab1a88ff1b8ebcd986c2a
Kerem Korkmaz <kerem.493@hotmail.com>
2021-10-10 19:15:23 +0300

    10.10.2021(MyDictionaryOdevi)

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyDictionary<K,V>
+    {
+
+        K[] keyDictionary;
+        V[] valueDictionary;
+        public MyDictionary()
+        {
+            keyDictionary = new K[0];
+            valueDictionary = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKey = keyDictionary;
+            V[] tempValue = valueDictionary;
+            keyDictionary = new K[tempKey.Length + 1];
+            valueDictionary = new V[tempValue.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keyDictionary[i] = tempKey[i];
+                valueDictionary[i] = tempValue[i];
+            }
+
+            keyDictionary[keyDictionary.Length - 1] = key;
+            valueDictionary[valueDictionary.Length - 1] = value;
+        }
+
+    }
+}
